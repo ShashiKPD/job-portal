03f997ae2b90afc16613e3c30e56790151427346
Shashikant Prasad <shashikantprasad107@gmail.com>
2025-01-27 03:01:38 +0530

    user model, routes, controllers added. Utils added

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,15 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+
+const register = asyncHandler(async (req, res) => {
+
+
+});
+
+const login = asyncHandler(async (req, res) => {
+
+
+});
+
+
+export { register, login };
